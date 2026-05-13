e3c5b9c37db6834e17fb0b982f2173c86c22117d
Vijay Pachlegaonkar <vnpachlegaonkar.nbnscoe@gmail.com>
2026-05-13 23:14:56 +0530

    comment added in testrunner

--- src/test/java/com/qa/runners/TestRunner.java
+++ src/test/java/com/qa/runners/TestRunner.java
@@ -3,6 +3,8 @@ package com.qa.runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+// This file will be the starting point for executing all the test cases. 
+
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = {"com.qa.stepdefinitions","com.qa.hooks"}, 
