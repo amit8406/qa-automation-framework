aef0d998c6678890343bca8930e2249fe50a85dd
Amit Tiparadi <amit.tiparadi1@gmail.com>
2026-05-13 22:47:45 +0530

    TestRunner File is added

---
+++ src/test/java/com/qa/runners/TestRunner.java
@@ -0,0 +1,18 @@
+package com.qa.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"com.qa.stepdefinitions","com.qa.hooks"}, 
+    plugin = {
+    		"pretty",
+    	    "html:target/cucumber-report.html",
+    	   },
+    monochrome = true
+   
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
